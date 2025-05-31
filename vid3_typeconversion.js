77e69a1018faf17a34e26d7bb527eedd0c10973c
Sarav <Saravjeetofficial@gmail.com>
2025-05-31 11:38:56 +0530

    type conversion examples and notes

--- vid3_typeconversion.js
+++ vid3_typeconversion.js
@@ -5,3 +5,9 @@ let valueInNumber = Number(score)
 console.log(typeof valueInNumber) //number
 
 //let a = "2451abc" can be converted to number like in above example but when the type is check it will give nan (not a number)
+//true =1, false = 0, if value is empty then it is false when converted to boolean otherwise true
+
+let isloggedin = 1
+let booleanlogged = Boolean(isloggedin)  //true  
+console.log(booleanlogged)
+console.log(typeof booleanlogged)
