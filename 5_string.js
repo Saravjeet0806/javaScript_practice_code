98fca655e4451c8e010de5eacfdcd1b0bc62c174
Sarav <Saravjeetofficial@gmail.com>
2025-06-01 18:56:28 +0530

    string in js

---
+++ 5_string.js
@@ -0,0 +1,19 @@
+const name  = "Saravjeet"
+const rollno = 29
+
+console.log(`Hello, my name is ${name} and my rollno is ${rollno}`) //use this way to use strings in js
+
+const str1 = new String("helloworld-IamSaravjeet")
+
+console.log(str1)
+
+console.log(str1.charAt(4))
+console.log(str1.toUpperCase())
+console.log(str1.length)
+
+const str2 = "   sarav    "
+console.log(str2.trim())
+
+console.log(str2.replace("a", "b"))
+
+//similarly use string functions like include, substring, slice ... use docs to explore more concepts
